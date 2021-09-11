388cf4164b29fbc6bd4577af489434be912902eb
Abir zaid contreras von Boeck <abircontreras@gmail.com>
2021-09-11 18:10:15 -0300

    Primeras clases, propiedades y métodos

---
+++ BombermanUSFX/Bomber.h
@@ -0,0 +1,15 @@
+#pragma once
+class Bomber
+{
+private:
+	int altoAvatar;
+	int anchoAvatar;
+
+public:
+	int getAnchoAvatar() { return anchoAvatar; }
+	void setAnchoAvatar(int _anchoAvatar) { anchoAvatar = _anchoAvatar; }
+
+	int getAltoAvatar() { return altoAvatar; }
+	void setAltoAvatar(int _altoAvatar) { altoAvatar = _altoAvatar; }
+};
+
